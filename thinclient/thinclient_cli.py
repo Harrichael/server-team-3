b260421cab9264a09acd851511a89fae19f5a825
Logan N <loganoggie@gmail.com>
2017-04-20 09:51:54 -0500

    worked on the thinclient cli file

--- thinclient/thinclient_cli.py
+++ thinclient/thinclient_cli.py
@@ -27,19 +27,19 @@ class Thinclient(cmd.Cmd):
         }
         self.api.post('/session', data)
 
-    @tokenize       """ ! """
+    @tokenize
     @num_tokens(1)
     def do_logout(self):
         self.api.delete('/session')
 
-    @tokenize       """ ! """
+    @tokenize 
     @num_tokens(0)
-    def get_users(self):
+    def do_get_online_users(self):
         self.api.get('/session/users')
 
     @tokenize
     @num_tokens(3)
-    def do_register(self, username, password, email):
+    def do_register_user(self, username, password, email):
         data = {
             'username': username,
             'password': password,
@@ -47,32 +47,32 @@ class Thinclient(cmd.Cmd):
         }
         self.api.post('/users', data)
 
-    @tokenize       """ ! """
+    @tokenize   
     @num_tokens(2)
-    def change_password(self, session_key, password):
+    def do_change_password(self, session_key, password):
         data = {
             'session-key': session_key,
             'password': password
         }
         self.api.put('/users/<username>/password', data)
 
-    @tokenize       """ ! """
+    @tokenize   
     @num_tokens(2)
-    def verify_email(self, email, email_code):
+    def do_verify_email(self, email, email_code):
         data = {
             'email': email,
             'email_code': email_code
         }
         self.api.put('/users/<username>/emails', data)
 
-    @tokenize       """ ! """
+    @tokenize  
     @num_tokens(0)
-    def get_user_config(self):
+    def do_get_user_config(self):
         self.api.get('/users/<username>/config')
 
-    @tokenize       """ ! """
+    @tokenize  
     @num_tokens(4)
-    def update_user_config(self, ... ):
+    def do_update_user_config(self, ... ):
         data = {
 
         """argparse stuff"""
@@ -80,14 +80,14 @@ class Thinclient(cmd.Cmd):
         }
         self.api.put('/users/<username>/config')
 
-    @tokenize       """ ! """
+    @tokenize   
     @num_tokens(0)
-    def get_user_profile(self):
+    def do_get_user_profile(self):
         self.api.get('/users/<user>/profile')
 
-    @tokenize       """ ! """
+    @tokenize    
     @num_tokens(4)
-    def update_user_profile(self, ... ):
+    def do_update_user_profile(self, ... ):
         data = {
 
         """argparse stuff"""
@@ -95,57 +95,104 @@ class Thinclient(cmd.Cmd):
         }
         self.api.put('/users/<username>/profile', data)
 
-    @tokenize       """ ! """
+    @tokenize    
     @num_tokens(0)
-    def get_pm_history(self):
+    def do_get_pm_history(self):
         self.api.get('/users/<username>/pm/<user>')
 
-    @tokenize       """ ! """
+    @tokenize    
     @num_tokens(1)
-    def send_pm(self, message):
+    def do_send_pm(self, message):
         data = {
             'message': message
         }
         self.api.post('/users/<username>/pm/<user>', data)
 
-    @tokenize       """ ! """
+    @tokenize    
     @num_tokens(0)
-    def delete_pm(self):
+    def do_delete_pm(self):
         self.api.delete('/users/<username>/pm/<user>/<id>')
 
-    @tokenize       """ ! """
+    @tokenize
     @num_tokens(0)
-    def get_channel_list(self):
+    def do_get_channel_list(self):
         self.api.get('/channels')
 
-    @tokenize       """ ! """
+    @tokenize   
     @num_tokens(1)
-    def create_channel(self, channel_name):
+    def do_create_channel(self, channel_name):
         data = {
             'channel_name': channel_name
         }
         self.api.post('/channels', data)
 
-    @tokenize       """ ! """
+    @tokenize  
     @num_tokens(0)
-    def delete_channel(self):
+    def do_delete_channel(self):
         self.api.delete('/channels/<channel>')
 
-    @tokenize       """ ! """
+    @tokenize   
     @num_tokens(0)
-    def get_channel_admins(self):
+    def do_get_channel_admins(self):
         self.api.get('/channels/<channel>/admins')
 
-    @tokenize       """ ! """
-    @num_tokens(?)
-    def adjust_admin(self, admins, chiefAdmin):
+    @tokenize   
+    @num_tokens(2)
+    def do_adjust_admin_level(self, admins, chiefAdmin):
         data = {
             'admins': admin_list,
             'chiefAdmin': admin
         }
         self.api.put('/channels/<channel>/admins', data)
 
-    
+    @tokenize    
+    @num_tokens(0)
+    def do_get_channel_subscribers(self):
+        self.api.get('/channels/<channel>/subscriptions')
+
+    @tokenize   
+    @num_tokens(0)
+    def do_subscribe_to_channel(self):
+        self.api.post('/channels/<channel>/subscriptions')
+
+    @tokenize  
+    @num_tokens(0)
+    def do_unsubscribe_to_channel(self):
+        self.api.delete('/channels/<channel>/subscriptions')
+
+    @tokenize      
+    @num_tokens(2)
+    def do_get_blocked_users(self):
+
+        self.api.get('/channels/<channel>/black-list')
+
+    @tokenize  
+    @num_tokens(0)
+    def do_block_user_channel(self, username, time):
+        data = {
+            'username': username,
+            'time': time
+        }
+        self.api.post('/channels/<channel>/black-list', data)
+
+    @tokenize
+    @num_tokens(0)
+    def do_get_channel_history(self):
+        self.api.get('/channels/<channel>/chat')
+
+    @tokenize
+    @num_tokens(1)
+    def do_send_message(self, message):
+        data = {
+            'message': message
+        }
+        self.api.post('/channels/<channel>/chat', data)
+
+    @tokenize
+    @num_tokens(0)
+    def do_delete_message(self):
+        self.api.delete('/channels/<channel>/chat/<id>')
+
 
 if __name__ == '__main__':
     Thinclient().cmdloop()
